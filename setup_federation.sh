2889523aa7c884a157d298e2ba6bc4ef5fd7660d
d-w-moore <dmoore@renci.org>
2019-10-26 11:08:52 -0400

    descriptive variable and raw output

--- setup_federation.sh
+++ setup_federation.sh
@@ -9,11 +9,11 @@ su_replace() {
 }
 
 irods_env_var() {
-    su - ${2:-irods} -c "jq .'$1' ~/.irods/irods_environment.json"
+    su - ${2:-irods} -c "jq -r .'$1' ~/.irods/irods_environment.json"
 }
 
 [ -r /tmp/federate_with.txt ] || exit 1
-typeset -A P=$(cat /tmp/federate_with.txt)
+typeset -A Remote=$(cat /tmp/federate_with.txt)
 
 SERVER_CONFIG=/etc/irods/server_config.json
 
@@ -24,10 +24,10 @@ if [[ ! $(jq ".federation?" $SERVER_CONFIG) =~ ^\ *\[\ *\]\ *$ ]]; then
 else
     EPOCH=`date +%s`
     jq '.federation+=[{
-        "catalog_provider_hosts": ["'${P[hn]}'"],
-        "negotiation_key": "'${P[nk]}'",
-        "zone_key":  "'${P[zk]}'",
-        "zone_name": "'${P[zn]}'",
+        "catalog_provider_hosts": ["'${Remote[hn]}'"],
+        "negotiation_key": "'${Remote[nk]}'",
+        "zone_key":  "'${Remote[zk]}'",
+        "zone_name": "'${Remote[zn]}'",
         }]' $SERVER_CONFIG > $SERVER_CONFIG.new &&\
         su_replace $SERVER_CONFIG ".new" ".previous.$EPOCH"
         STATUS=$?
@@ -38,14 +38,12 @@ su - irods -c '$HOME/irodsctl restart'
 
 su - irods -c "iadmin mkuser bobby rodsuser"
 su - irods -c "iadmin moduser bobby password bpass"
-su - irods -c "iadmin mkzone ${P[zn]} remote ${P[hn]}:${P[zp]}"
+su - irods -c "iadmin mkzone ${Remote[zn]} remote ${Remote[hn]}:${Remote[zp]}"
 
 IRODS_USER_NAME=$(irods_env_var irods_user_name)
 IRODS_ZONE_NAME=$(irods_env_var irods_zone_name)
-REMOTE_USER="bobby#${P[zn]}"
+REMOTE_USER="bobby#${Remote[zn]}"
 REMOTE_USER_HOME="/$IRODS_ZONE_NAME/home/$IRODS_USER_NAME/$REMOTE_USER"
-
 
 su - irods -c "imkdir $REMOTE_USER_HOME"
 su - irods -c "ichown write $REMOTE_USER $REMOTE_USER_HOME"
-
